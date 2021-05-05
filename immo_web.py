7cdd291aa46564bf2d845e5c37ce21deddc3ec86
Jeremy Lipszyc <jeremylipszyc@gmail.com>
2021-05-05 16:15:27 +0200

    Update immo_web.py

--- immo_web.py
+++ immo_web.py
@@ -1,25 +1,3 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
-from bs4 import BeautifulSoup
-
-def return_one_page():
-
-    url = "https://www.immoweb.be/fr/annonce/immeuble-a-appartements/a-vendre/bruxelles-ville/1000/9302481?searchId=6091399b6e08f"
-    driver = webdriver.Chrome()
-    driver.get(url)
-    return driver.page_source
-
-if __name__=='__main__':
-    html = return_one_page()
-
-
 def b_soup_immo_web(html):
     soup = BeautifulSoup(html,'lxml')
     json = soup.find_all("script")
@@ -30,17 +8,26 @@ def b_soup_immo_web(html):
     #json_object = js.dumps(json_clean)
     data = js.loads(json_clean)
     print(data)
+    adid, transaction_type, type_prop, subtype, zipcode = None, None, None, None, None
     
-    adid = data["classified"]["id"]
-    transaction_type = data["classified"]["transactionType"]
-    type_prop = data["classified"]["type"]
-    subtype = data["classified"]["subtype"]
-    zipcode = data["classified"]["zip"]
-    etat = data["classified"]["building"]["condition"]
+    variables= [adid, transaction_type, type_prop, subtype, zipcode]
+    mot_cle = ["id", "transactionType", "type", "subtype", "zip"]
+    
+    for i in range(len(variables)):
+        print(data["classified"][mot_cle[i]])
+        try:
+            variables[i] = data["classified"][mot_cle[i]]
+        except:
+            print("Exception")
+            variables[i] = None
+        print(variables[i])
+    
+    try:
+        etat = data["classified"]["building"]["condition"]
+    except:
+        etat = None
     
     
-    print(adid, bedrooms, price, transaction_type, subtype, zipcode, type_prop,
-    etat, garden, garden_surface)
     
     """headers = soup.find_all("th", {"class":"classified-table__header"})
     
@@ -60,8 +47,6 @@ def b_soup_immo_web(html):
         elif text == "Quartier ou lieu-dit":
             = header.find_next_sibling().text.strip()"""
         
-        
-    #print(facades, surface_habitable)
 
 def find_brackets(text):
     index1, index2 = -1, -1
